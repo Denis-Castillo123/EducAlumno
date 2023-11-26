e78f4a450da0acad4cedc2d86e54f3b1e66ff4d9
Denis-Castillo123 <90869371+Denis-Castillo123@users.noreply.github.com>
2023-11-26 02:48:49 -0600

    Implementación total de los enemigos, generación random, extablecimiento de puntos de spawn y litimar la cantidad de enemigos

--- Educalumnos/Assets/Scripts/Barron_Mov.cs
+++ Educalumnos/Assets/Scripts/Barron_Mov.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Barron_Mov : MonoBehaviour
 {
+    public GameObject AtaqueE;
     public float Speed;
     public float JumpForce;
     public float JumMax;
@@ -27,48 +28,68 @@ public class Barron_Mov : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Movimiento del personaje
         Horizontal = Input.GetAxisRaw("Horizontal");
 
+        //Establece la dirección en la cual el personaje estara avanzando
         if (Horizontal < 0.0f) transform.localScale = new Vector2 (-10.0f, 10.0f);
         else if (Horizontal > 0.0f) transform.localScale = new Vector2(10.0f, 10.0f);
 
+        //Llamada de las animaciones de caminar y Suelo
         Animator.SetBool("Caminando", Horizontal != 0.0f);
+        //Determina base en el escenario
         Animator.SetBool("EnSuelo",Suelo());
         
         Debug.DrawRay(transform.position, Vector2.down * 0.1f, Color.red);
 
         Jump();
+
+        // if(Input.GetKey(KeyCode.Space))
+        // {
+        //     Shoot();
+        // }
     }
 
+    //Metodo para establecer un suelo en el nivel
     bool Suelo(){
         RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, new Vector2(boxCollider.bounds.size.x,boxCollider.bounds.size.y), 0f, Vector2.down, 0.2f, CapaSuelo);
         return raycastHit.collider != null;
     }
 
+    //Metodo para saltar el personaje
     private void Jump()
     {
-        
-        // Rigidbody2D.AddForce(Vector2.up * JumpForce);
+        //Determina el limite de saltos del personaje
         if(Suelo()){
-//            Animator.SetBool("Salto", false);
+
             JumpRest = JumMax;
         }
 
         if (Input.GetKeyDown(KeyCode.W) && JumpRest > 0)
         {
             JumpRest--;
-//           Animator.SetBool("Salto", true);
             Rigidbody2D.velocity = new Vector2(Rigidbody2D.velocity.x,0f);
             Debug.Log("Jumping!");
             Rigidbody2D.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
         }
     }
 
+    //Metodo para establecer la velocidad y el posicion limite del personaje
     private void FixedUpdate()
     {
         Rigidbody2D.velocity = new Vector2(Horizontal * Speed, Rigidbody2D.velocity.y);
         Rigidbody2D.position = new Vector2(Mathf.Clamp(Rigidbody2D.position.x, -8.28f, 43.96f),
             Rigidbody2D.position.y);
     }
+
+    // private void Shoot()
+    // {
+    //     Vector2 direction;
+    //     if(transform.localScale.x == 1.0f) direction = Vector2.right;
+    //     else direction = Vector2.left;
+
+    //     GameObject ataque = Instantiate(BulletPrefab,transform.position + direction * 0.1f ,Quaternion.identity);
+    //     ataque.GetComponent<power>().SetDirection(direction);
+    // }
 
 }
